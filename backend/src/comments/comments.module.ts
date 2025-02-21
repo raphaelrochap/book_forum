bead728e22a3804ba375100830d551a4dbfbe578
Raphael da Rocha <raphaelrochap@gmail.com>
2025-02-21 05:16:03 -0300

    Implementado envio de email ao criar comentário.

--- backend/src/comments/comments.module.ts
+++ backend/src/comments/comments.module.ts
@@ -3,11 +3,18 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { CommentEntity } from './entities/comment.entity';
 import { CommentsService } from './comments.service';
 import { CommentsController } from './comments.controller';
+import { UsersService } from 'src/users/users.service';
+import { UserEntity } from 'src/users/entities/user.entity';
+import { PostsService } from 'src/posts/posts.service';
+import { PostEntity } from 'src/posts/entities/post.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([CommentEntity])],
+  imports: [
+    TypeOrmModule.forFeature([CommentEntity]),
+    TypeOrmModule.forFeature([UserEntity]),
+    TypeOrmModule.forFeature([PostEntity])],
   controllers: [CommentsController],
-  providers: [CommentsService],
+  providers: [CommentsService, UsersService, PostsService],
 })
 
 export class CommentsModule {}
--- backend/src/comments/comments.service.ts
+++ backend/src/comments/comments.service.ts
@@ -3,13 +3,42 @@ import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { CommentEntity } from './entities/comment.entity';
 import { Repository } from 'typeorm';
+import * as nodemailer from 'nodemailer'
+import { UsersService } from 'src/users/users.service';
+import { UserEntity } from 'src/users/entities/user.entity';
+import { PostsService } from 'src/posts/posts.service';
+import { PostEntity } from 'src/posts/entities/post.entity';
 
 @Injectable()
 export class CommentsService {
   constructor(
       @InjectRepository(CommentEntity)
-      private commentsRepository: Repository<CommentEntity>
+      private commentsRepository: Repository<CommentEntity>,      
+      private usersService: UsersService,
+      private postsService: PostsService
   ) {}
+
+  async sendEmail(to: string, subject: string, text: string) {
+    const transporter = nodemailer.createTransport({
+      service: 'gmail',
+      auth: {
+        user: process.env.EMAIL_USER,
+        pass: process.env.EMAIL_PASS,
+      },
+    });
+
+    try {
+      await transporter.sendMail({
+        from: `"Book Forum" <${process.env.EMAIL_USER}>`,
+        to,
+        subject,
+        text,
+      });
+      console.log('Email enviado com sucesso!');
+    } catch (error) {
+      console.error('Erro ao enviar email:', error);
+    }
+  }
 
   findAll(): Promise<CommentEntity[]> {
     return this.commentsRepository.find({
@@ -29,8 +58,17 @@ export class CommentsService {
     return comments.filter((comment) => (comment.post_id.id == id))
   }
 
-  save(comment: CommentEntity): Promise<CommentEntity> {
-    return this.commentsRepository.save(comment);
+  async save(comment: CommentEntity): Promise<CommentEntity> {
+    const post: PostEntity = await this.postsService.findOneById(Number(comment.post_id))
+    const userPost: UserEntity = await this.usersService.findOneById(Number(post.user_id.id))
+    const userComment: UserEntity = await this.usersService.findOneById(Number(comment.user_id))
+    this.sendEmail(
+      userPost.email,
+      'Book Fórum - Alguém comentou em sua publicação!',
+      `Olá, ${userPost.name}! Fique de olho na sua rede, um novo comentário foi adicionado por ${userComment.name} na sua publicação. Venha ver! :)`
+    )
+
+    return this.commentsRepository.save(comment);    
   }
 
   async remove(id: number, user_id: number) {
